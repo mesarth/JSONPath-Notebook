a23c04fd0f61d92ab824f09fde92c8b37734b44f
Tobias Schranz <tobias.g.schranz@gmail.com>
2023-07-29 12:04:08 +0200

    test: openEmptyNotebook

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,26 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import { EXTENSION_ID, NOTEBOOK_TYPE } from '../../utils';
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
+suite('Extension Test Suite', async () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	suite('Command[openEmptyNotebook]', async () => {
+		beforeEach(async () => {
+			//close all open tabs
+			await vscode.commands.executeCommand('workbench.action.closeAllEditors');
+		});
+
+		test('is correct NotebookType', async () => {
+			await vscode.commands.executeCommand(`${EXTENSION_ID}.openEmptyNotebook`);
+			assert.equal(NOTEBOOK_TYPE, vscode.window.activeNotebookEditor?.notebook.notebookType);
+		});
+
+		test('is correct NotebookType', async () => {
+			await vscode.commands.executeCommand(`${EXTENSION_ID}.openEmptyNotebook`);
+			assert.equal(2, vscode.window.activeNotebookEditor?.notebook.cellCount);
+		});
 	});
 });

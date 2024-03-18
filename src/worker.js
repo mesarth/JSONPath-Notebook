cd80fc825bbc25c20b78ce572bd0225643b33d74
Tobias Schranz <tobias.g.schranz@gmail.com>
2024-03-18 22:37:16 +0100

    use json-p3 as engine

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,6 @@
 import { parentPort, workerData } from 'worker_threads';
-import jsonPath from './lib/jsonpath';
+import { jsonpath } from "json-p3";
 
-const result = jsonPath(workerData.input, workerData.expression);
-parentPort.postMessage(result);
+const result = jsonpath.query(workerData.expression, workerData.input);
+parentPort.postMessage(result.valuesOrSingular());
 process.exit();

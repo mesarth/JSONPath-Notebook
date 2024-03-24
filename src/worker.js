2cff1987bab87340f318356d529e6d88b2f88ebc
Tobias Schranz <tobias.g.schranz@gmail.com>
2024-03-24 18:08:30 +0100

    return array even for single node result

--- src/worker.js
+++ src/worker.js
@@ -2,5 +2,5 @@ import { parentPort, workerData } from 'worker_threads';
 import { jsonpath } from "json-p3";
 
 const result = jsonpath.query(workerData.expression, workerData.input);
-parentPort.postMessage(result.valuesOrSingular());
+parentPort.postMessage(result.values());
 process.exit();
